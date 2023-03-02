84199bff104a14538ad85b7ca1473d3e4665b17e
RHsanto <rakibulsanto789@gmail.com>
2023-03-02 20:50:34 +0600

    add initial set up

---
+++ components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import About from "./About";
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="">Home Page</h1>
+      <About/>
+    </div>
+  );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'primary': '#503AE7',
+      'secondary': '#1AD993',
+      'Aquamarine': '#ABFFE0',
+      'Purple': '#832BC1',
+      'gray': '#AFB0B9',
+      'off-white': '#F4F2FF',
+      'black':'#14142B'
+    },
+  
     extend: {},
   },
   plugins: [],
